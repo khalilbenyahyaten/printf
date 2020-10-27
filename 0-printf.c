90e1b5494f710874def492ab5877dbcbf0279fd2
eya-98 <eya.nani@sesame.com.tn>
2020-10-27 13:51:48 +0000

    printf

--- 0-printf.c
+++ 0-printf.c
@@ -1,40 +1,67 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 /**
  * _printf - produces output according to a format.
  * @format : const char
- */
+ */ 
 int _printf(const char *format, ...)
 {
-char *input, *str;
-int i;
-int j = 0;
+char *str = "", *string = "", *ch = "", *final;
+int i, j, k, l = 0, n, m;
 va_list valist;
 va_start(valist, format);
+ i = 0;
+ l = 0;
 for (i = 0; format[i]; i++)
 {
-if (format[i] == "%")
+if (format[i] != '%')
 {
-str = format[i + 1];
-input = va_arg(valist, format);
-switch (str)
+str[l] = format[i];
+l++;
+}
+else
 {
-case 'c':
-format[i] = input;
+k = i;
+for (j = 0; j <= 1; j++)
+{
+ch[j] = format[k];
+k++;
+}
+switch (ch[1])
+{
+case '%':
+{
+str[l] = '%';
+l++;
+i = i + 2;
+break;
+}
 case 's':
-while (input[j] != '\0')
 {
-format[i] = input[j];
-j++;
-i++;
-}
-}
-}
-}
-for (i = 0; format[i]; i++)
+string = va_arg(valist, char *);
+for ( n = 0; string[n]; n++)
 {
+str[l] = string[n];
 }
+l = l + n - 1;
+break;
+}
+case 'c':
+{
+str[l] = va_arg(valist, int);
+l++;
+break;
+}
+}
+}
+}
+final = malloc(sizeof(char) * l);
+for (m = 0; m <= l; m++)
+final[m] = str[m];
+puts(final);
+free (final);
 va_end(valist);
-return (i);
+return (l);
 }
